6f70745d268ad375bd7fdc71045dd4e8cf9e59fc
Hussein Najah <husseinnajah123@gmail.com>
2023-10-13 23:16:55 +0300

    page + layout

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,13 +1,15 @@
 import { Heroes } from "./_components/heroes";
+import { Footer } from "./_components/footer";
 import { Heading } from "./_components/heading";
 
 const MarketingPage = () => {
   return (
-    <div className="min-h-full flex flex-col">
-      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+    <div className="flex flex-col min-h-full">
+      <div className="md:justify-start gap-y-8 flex flex-col items-center justify-center flex-1 px-6 pb-10 text-center">
         <Heading />
         <Heroes />
       </div>
+      <Footer />
     </div>
   );
 };
