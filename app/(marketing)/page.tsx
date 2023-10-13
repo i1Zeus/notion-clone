75db196accabe9926f2d0ef58fa698d82768c4b0
Hussein Najah <husseinnajah123@gmail.com>
2023-10-13 21:50:04 +0300

    logo dark + light

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <div>Click me!</div>
+      <div>marketing</div>
     </main>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Zotion",
   description: "Notion clone built with Next.js and Tailwind CSS",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/Logo-Dark.svg",
+        href: "/Logo-Dark.svg",
+      },
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/Logo.svg",
+        href: "/Logo.svg",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
