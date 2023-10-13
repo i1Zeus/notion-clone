e421640b6bd9f0a2f90353e70558d679390d9193
Hussein Najah <husseinnajah123@gmail.com>
2023-10-13 22:52:50 +0300

    images

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { Heroes } from "./_components/heroes";
+import { Heading } from "./_components/heading";
+
+const MarketingPage = () => {
   return (
-    <main>
-      <div>marketing</div>
-    </main>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
+        <Heading />
+        <Heroes />
+      </div>
+    </div>
   );
-}
+};
+export default MarketingPage;
