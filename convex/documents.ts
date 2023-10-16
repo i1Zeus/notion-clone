cc4e0e58e62310d747690cfce4358e63dda40f8d
Hussein Najah <husseinnajah123@gmail.com>
2023-10-16 21:51:52 +0300

    delete mutation

--- convex/documents.ts
+++ convex/documents.ts
@@ -143,10 +143,29 @@ export const restore = mutation({
       if (parent?.isArchived) options.parentDocument = undefined;
     }
 
-    await ctx.db.patch(args.id, options);
+    const document = await ctx.db.patch(args.id, options);
 
     recursiveRestore(args.id);
 
-    return existingDocument;
+    return document;
+  },
+});
+
+export const remove = mutation({
+  args: { id: v.id("documents") },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if (!identity) throw new Error("Not authenticated");
+
+    const userId = identity.subject;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if (!existingDocument) throw new Error("Document not found");
+
+    if (existingDocument.userId !== userId) throw new Error("Unauthorized");
+
+    const document = await ctx.db.delete(args.id);
+
+    return document;
   },
 });
