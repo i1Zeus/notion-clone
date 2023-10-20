8c07618714f42c4172b93e936153e73f47dcc1b7
Hussein Najah <husseinnajah123@gmail.com>
2023-10-20 23:21:51 +0300

    mx

--- app/(main)/(routes)/documents/[documentId]/page.tsx
+++ app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -49,7 +49,7 @@ const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
   return (
     <div className="pb-40">
       <Cover url={document.coverImage} />
-      <div className="md:max-w-3xl lg:max-w-4xl ">
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
         <Toolbar initialData={document} />
         <Editor onChange={onChange} initialContent={document.content} />
       </div>
